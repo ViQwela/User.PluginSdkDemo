e562530f25f1643c5388418791f9a61fc273fba0
Victorio <160603162+ViQwela@users.noreply.github.com>
2025-08-15 01:09:16 +0400

    Add files via upload

---
+++ SettingsControlDemo.xaml.cs
@@ -0,0 +1,46 @@
+﻿using SimHub.Plugins.Styles;
+using System.Windows.Controls;
+
+namespace User.PluginSdkDemo
+{
+    /// <summary>
+    /// Logique d'interaction pour SettingsControlDemo.xaml
+    /// </summary>
+    public partial class SettingsControlDemo : UserControl
+    {
+        public DataPluginDemo Plugin { get; }
+
+        public SettingsControlDemo()
+        {
+            InitializeComponent();
+        }
+
+        public SettingsControlDemo(DataPluginDemo plugin) : this()
+        {
+            this.Plugin = plugin;
+        }
+
+        private async void StyledMessageBox_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var res = await SHMessageBox.Show("Message box", "Hello", System.Windows.MessageBoxButton.OKCancel, System.Windows.MessageBoxImage.Question);
+
+            await SHMessageBox.Show(res.ToString());
+        }
+
+        private void DemoWindow_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var window = new DemoWindow();
+
+            window.Show();
+        }
+
+        private async void DemodialogWindow_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var dialogWindow = new DemoDialogWindow();
+
+            var res = await dialogWindow.ShowDialogWindowAsync(this);
+
+            await SHMessageBox.Show(res.ToString());
+        }
+    }
+}
